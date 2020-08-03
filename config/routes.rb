b1a4d37aa2cd2de6d3b70f73917ef46e25422eb5
sonotaku <takumasonoda0223@icloud.com>
2020-08-03 11:17:10 +0900

    トップページの表示を実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  get 'orders/index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'orders#index'
+
 end

c65b3a5c1b14c93a6fd2b249f9b113921e033c46
sonotaku <takumasonoda0223@icloud.com>
2020-08-03 16:47:44 +0900

    リダイレクトの設定

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,11 @@
 class OrdersController < ApplicationController
+  before_action :move_to_index, except: [:index]
   def index
   end
 
-  def new
+  private
+
+  def move_to_index
+    redirect_to action: :index unless user_signed_in?
   end
 end

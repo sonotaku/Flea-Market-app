35aca007ab3a31a20054080f68cb539149639fd8
sonotaku <takumasonoda0223@icloud.com>
2020-08-04 19:26:10 +0900

    emailのバリデーション を変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :email, presence: true, uniqueness: true, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  validates :email, presence: true, uniqueness: true, format: { with: /@/ }
   # ６文字以上かつ半角英数字混合かつ２回入力
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
   validates_format_of :password, presence: true, length: { minimum: 6 }, with: PASSWORD_REGEX

6d344b60dfaac3b9961aca5a504ee9952a04c983
sonotaku <takumasonoda0223@icloud.com>
2020-08-05 12:42:16 +0900

    共通のバリデーションをまとめて記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,17 +3,20 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-  validates :nickname, presence: true
-  validates :email, presence: true, uniqueness: true, format: { with: /@/ }
-  # ６文字以上かつ半角英数字混合かつ２回入力
-  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates_format_of :password, presence: true, with: PASSWORD_REGEX
-  # 全角のみ（ひらがな、カタカナ、漢字）
-  validates :family_name, presence: true, format: { with: /\A[ぁ-んァ-ンー-龥]/ }
-  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ンー-龥]/ }
-  # 全角カタカナのみ
-  validates :flat_family_name, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
-  validates :flat_first_name, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
-  validates :birthday, presence: true
+# presence trueをまとめて記述
+  with_options presence: ture do
+    
+    validates :nickname
+    validates :email, uniqueness: true, format: { with: /@/ }
+    # ６文字以上かつ半角英数字混合かつ２回入力
+    PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+    validates_format_of :password, with: PASSWORD_REGEX
+    # 全角のみ（ひらがな、カタカナ、漢字）
+    validates :family_name, format: { with: /\A[ぁ-んァ-ンー-龥]/ }
+    validates :first_name, format: { with: /\A[ぁ-んァ-ンー-龥]/ }
+    # 全角カタカナのみ
+    validates :flat_family_name, format: { with: /\A[ァ-ヶー－]+\z/ }
+    validates :flat_first_name, format: { with: /\A[ァ-ヶー－]+\z/ }
+    validates :birthday
+  end
 end

5a61371577966905983742ae25be1e489db651d1
sonotaku <takumasonoda0223@icloud.com>
2020-08-05 11:48:38 +0900

    factorybotの修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,13 +1,28 @@
 FactoryBot.define do
   factory :user do
-    nickname =  Faker::Name.last_name 
-    family_name = Gimei.name.last.kanji 
-    first_name = Gimei.name.first.kanji 
-    flat_family_name = Gimei.name.last.katakana 
-    flat_first_name = Gimei.name.first.katakana 
+    nickname {Faker::Name.last_name} 
+    family_name {"山田"}
+    first_name {"太郎"}
+    flat_family_name {"ヤマダ"}
+    flat_first_name {"タロウ"}
     email { Faker::Internet.free_email }
     password = Faker::Internet.password(min_length: 6)
     password {password}
     password_confirmation {password}
+    birthday {"2000-2-23"}
+
+    # Gimeiを使ったテスト用Factory Bot（なぜかGimei使えない）
+
+    # gimei = Gimei.name
+    # nickname {Faker::Name.last_name} 
+    # family_name = gimei.last.kanji 
+    # first_name = gimei.first.kanji
+    # flat_family_name = gimei.last.katakana 
+    # flat_first_name = gimei.first.katakana
+    # email { Faker::Internet.free_email }
+    # password = Faker::Internet.password(min_length: 6)
+    # password {password}
+    # password_confirmation {password}
+    # birthday {"2000-2-23"}
   end
 end

407f0a2bcc025fa0cf43a6455a0b9352496d1121
sonotaku <takumasonoda0223@icloud.com>
2020-08-06 12:24:08 +0900

    バリデーション ,アソシエーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,20 @@
 class Item < ApplicationRecord
-  
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category, :condition, :burden, :prefecture, :day
+  belongs_to :user
+
+  with_options presence: true do
+    validates :image, 
+    validates :name
+    validates :descriptiom
+    validates :category_id, numericality: { other_than: 1 } 
+    validates :condition_id, numericality: { other_than: 1 } 
+    validates :burden_id, numericality: { other_than: 1 } 
+    validates :prefecture_id, numericality: { other_than: 0 } 
+    validates :day_id, numericality: { other_than: 1 } 
+    validates :price, :numericality => {
+      :greater_than_or_equal_to => 300,
+      :less_than_or_equal_to => 9999999,
+    validates :user
+  end
 end

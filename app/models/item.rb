50c858fd122e2ed5682b230b40c54ca64eae03a0
sonotaku <takumasonoda0223@icloud.com>
2020-08-06 14:45:40 +0900

    モデルの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,10 +1,14 @@
 class Item < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
-  belongs_to_active_hash :category, :condition, :burden, :prefecture, :day
+  belongs_to_active_hash :category
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :burden
+  belongs_to_active_hash :prefecture
+  belongs_to_active_hash :day
   belongs_to :user
 
   with_options presence: true do
-    validates :image, 
+    validates :image
     validates :name
     validates :descriptiom
     validates :category_id, numericality: { other_than: 1 } 
@@ -14,7 +18,7 @@ class Item < ApplicationRecord
     validates :day_id, numericality: { other_than: 1 } 
     validates :price, :numericality => {
       :greater_than_or_equal_to => 300,
-      :less_than_or_equal_to => 9999999,
+      :less_than_or_equal_to => 9999999}
     validates :user
   end
 end

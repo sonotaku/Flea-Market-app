641b8ad5504dd7e52f66d894e5bf5367160c7dbb
sonotaku <takumasonoda0223@icloud.com>
2020-08-06 14:22:48 +0900

    コントローラーにcreateアクションを定義

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -8,10 +8,23 @@ class OrdersController < ApplicationController
   def new
     @item = Item.new
   end
+  
+  def create
+    @item = Item.new(item_params)
+    if @item.save!
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
 
   private
 
   def move_to_index
     redirect_to action: :index unless user_signed_in?
+  end
+
+  def item_params
+    parems.require(:item).permit(:image, :name, :description, :category_id, :condition_id, :burden_id, :prefecture_id, :day_id, :price).merge(user: current_user.id)
   end
 end

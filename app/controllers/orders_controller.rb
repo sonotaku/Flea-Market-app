82eeb2afcc93a486ff3fe1557d194e1e5674b37f
sonotaku <takumasonoda0223@icloud.com>
2020-08-07 21:34:49 +0900

    修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -12,36 +12,15 @@ class OrdersController < ApplicationController
       redirect_to root_path
     end
   end
-
+  
   def create
-    if  params[:item_image].present?
-      @item = Item.new(item_params)
-      respond_to do |format|
-        format.html
-        format.json {render json: @item}
-      end
-      if @item.save
-        # 複数画像が入ったハッシュ形式からひとつずつ取り出して保存
-        params[:item_image][:image].each do |image|
-          @item.item_image.create(image: image)
-        end
-        redirect_to item_path(@item)
-      else
-        redirect_to new_order_path
-      end
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
     else
-      redirect_to new_order_path
+      render :new
     end
   end
-  
-  # def create
-  #   @item = Item.new(item_params)
-  #   if @item.save
-  #     redirect_to root_path
-  #   else
-  #     render action: :new
-  #   end
-  # end
 
   private
   

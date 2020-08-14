8f69ebd6abacdb029456e81d36ec3ffb0a9faf6a
sonotaku <takumasonoda0223@icloud.com>
2020-08-14 17:37:57 +0900

    同じ処理をset_itemとしてまとめた

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-
+  before_action :set_item, only: [:show, :edit]
   def index
     @items = Item.all
   end
@@ -23,11 +23,9 @@ class ItemsController < ApplicationController
   end
   
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
@@ -55,5 +53,9 @@ class ItemsController < ApplicationController
       :day_id, 
       :price
       ).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end

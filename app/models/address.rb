34606a9e028188d2502dc6602afa6896ec82c4f9
sonotaku <takumasonoda0223@icloud.com>
2020-08-18 12:11:58 +0900

    addressモデルのバリデーションの設定

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,2 +1,13 @@
 class Address < ApplicationRecord
+  belongs_to :item
+
+  with_options presence: true do
+    validates :postal_number, format: { with: /-/ }   # ハイフン必須
+    validates :prefecture_id, numericality: { other_than: 0 }   # 0より大きいもの
+    validates :city
+    validates :address
+    validates :phone, format: { with: /\A\d{11}\z/}  # ハイフンなし、11文字以内
+    validates :item
+  end
+  validates :building
 end

e53a3ba3060641bdaeb2c04681bb35bfdf157712
sonotaku <takumasonoda0223@icloud.com>
2020-08-27 16:44:50 +0900

    削除に関するエラーハンドリング設定

--- app/controllers/purchase_items_controller.rb
+++ app/controllers/purchase_items_controller.rb
@@ -1,8 +1,13 @@
 class PurchaseItemsController < ApplicationController
+  before_action :move_to_root, only: [:index, :create]
 
     # 購入確認画面表示
   def index
+    if user_signed_in?
     @item = Item.find_by(id: params[:item_id])
+    else
+      redirect_to new_user_session_path
+    end
   end
 
     # 購入
@@ -46,5 +51,14 @@ class PurchaseItemsController < ApplicationController
         card: purchase_params[:token],    # カードトークン
         currency:'jpy'                 # 通貨の種類
       )
+    end
+
+    # 出品者がURLを直接入力して購入ページに遷移 → トップページに遷移
+    # URLを直接入力して購入済み商品の購入ページへ遷移 → トップページに遷移
+    def move_to_root
+      @item = Item.find_by(id: params[:item_id])
+      if current_user.id == @item.user_id || @item.buyer_id.present?
+      redirect_to root_path 
+      end
     end
 end
